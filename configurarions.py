11270c51b29d04e8002847d310a0ebbebe877c54
Yaswanth <yashmanic96@gmail.com>
2025-11-20 21:47:53 +0530

    Mongo Url Updation

--- configurarions.py
+++ configurarions.py
@@ -2,10 +2,18 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
+import certifi  # <--- 1. Import this library
+
 load_dotenv()
+
 uri = os.getenv("MONGO_URI")
-DB = os.getenv("DB_NAME")
-client = MongoClient(uri,server_api=ServerApi("1"))
-db=client.DB
+DB_NAME = os.getenv("DB_NAME")  # (Renamed variable slightly for clarity)
+
+# 2. Add tlsCAFile=certifi.where() to fix the SSL Handshake Error
+client = MongoClient(uri, server_api=ServerApi("1"), tlsCAFile=certifi.where())
+
+# 3. Fix: Use dictionary syntax client[DB_NAME] to use the actual string from your .env
+db = client[DB_NAME]
+
 collection = db["users"]
 review_collection = db["review"]
